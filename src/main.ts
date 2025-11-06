84b2cb7dd1ad58c5573ae5676d6e844b862883cc
Prakash <qxprakash@gmail.com>
2025-11-07 02:16:15 +0530

    cleanup

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,6 @@ import type { Meta, Body } from '@uppy/core'
 const uppy = new Uppy<Meta, Body>()
 uppy.use(Dashboard<Meta,Body>)
 
-// Repro: TS2339 ("Property 'openModal' does not exist on type 'UnknownPlugin<...>'")
 const resultGetPlugin = uppy.getPlugin<Dashboard<Meta, Body>>('Dashboard')?.openModal()
 
 console.log(resultGetPlugin)
